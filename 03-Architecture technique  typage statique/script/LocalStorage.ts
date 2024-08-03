1f0bd33bc65caa818304e960c2de585846e6a3b3
Zorgos <pierre-louis.sans@edu.devinci.fr>
2024-08-03 11:30:29 +0200

    :lipstick: added some style & upgraded display logic

--- 03-Architecture technique  typage statique/script/LocalStorage.ts
+++ 03-Architecture technique  typage statique/script/LocalStorage.ts
@@ -6,6 +6,20 @@ class LocalStorage {
     }
 
     public static createProduct(product:Product):Product {
+        // check that the price is positive and above zero, check that the stock is positive and above or equal to zero, check that the rated is between 0 and 5
+        if(product.getPrice < 0) {
+            throw new Error('Price must be positive');
+        }
+
+        if(product.getStock < 0) {
+            throw new Error('Stock must be positive or equal to zero');
+        }
+
+        if(product.getRated < 0 || product.getRated > 5) {
+            throw new Error('Rated must be between 0 and 5');
+        }
+
+        // create the product
         let allProducts : any = localStorage.getItem('products') || '[]';
         allProducts = JSON.parse(allProducts);
         allProducts.push(product);
--- 03-Architecture technique  typage statique/script/app.ts
+++ 03-Architecture technique  typage statique/script/app.ts
@@ -8,15 +8,18 @@ try {
         console.log('localStorage empty');
         localStorage.setItem('products', JSON.stringify([]));
         let product1 = new Product('product1', 'description1', 100, 10, 5, true, 'image1', new Date(), new Date());
-        console.log(product1);
         let product2 = new Product('product2', 'description2', 200, 20, 4, true, 'image2', new Date(), new Date());
-        console.log(product2);
         let product3 = new Product('product3', 'description3', 300, 30, 3, true, 'image3', new Date(), new Date());
-        console.log(product3);
+        let product4 = new Product('product4', 'description4', 400, 0, 2, false, 'image4', new Date(), new Date());
+        let product5 = new Product('product5', 'description5', 500, 50, 1, true, 'image5', new Date(), new Date());
+        let product6 = new Product('product6', 'description6', 600, 60, 5, true, 'image6', new Date(), new Date());
 
         product1.createProduct();
         product2.createProduct();
         product3.createProduct();
+        product4.createProduct();
+        product5.createProduct();
+        product6.createProduct();
     }
 } catch (e) {
     console.error(e);
@@ -29,12 +32,38 @@ function displayProducts(array : any) {
         content += `
             <div class="product">
                 <h2>${product._title}</h2>
-                <p>About our product: ${product._description}</p>
-                <p>${product._price} $</p>
+                <div>
+                <img src="${product._image}" alt="${product._title}">
+                <ul>
+                    <li class="available"></li>
+                    <li>${product._price} $(USD)</li>
+                    <li>${product._rated}/5 ⭐</li>
+                    <li class="peremption">Expire in:</li>
+                    <li>About this product: ${product._description}</li>
+                </ul>
+                </div>
             </div>
         `;
     });
     container.innerHTML = content;
+    let Allperemption = document.querySelectorAll('.peremption');
+    Allperemption.forEach((peremption) => {
+        let expire = new Date();
+        let expireDate = new Date(expire.getFullYear(), expire.getMonth() + 1, expire.getDate());
+        let result = (expireDate.getTime() - expire.getTime()) / (1000 * 60 * 60 * 24);
+        result = Math.round(result);
+        peremption.innerHTML = 'Expire in: ' + result + ' days';
+    });
+    let Allavailable = document.querySelectorAll('.available');
+    let i = 0;
+    Allavailable.forEach((available) => {
+        if(array[i]._available) {
+            available.innerHTML = `${array[i]._stock} left in stock`;
+        } else {
+            available.innerHTML = 'Sorry we are out of stock';
+        }
+        i++;
+    });
 }
 
 let allProducts = LocalStorage.selectAllProduct();
