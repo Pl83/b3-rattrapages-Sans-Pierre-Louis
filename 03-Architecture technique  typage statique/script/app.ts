4c931ccd29a6eab45429be0b88d946ca0eb5d3ba
Zorgos <pierre-louis.sans@edu.devinci.fr>
2024-08-03 11:35:30 +0200

    added img link & lazy loading

--- 03-Architecture technique  typage statique/script/app.ts
+++ 03-Architecture technique  typage statique/script/app.ts
@@ -7,12 +7,12 @@ try {
     if(!localStorage.getItem('products')) {
         console.log('localStorage empty');
         localStorage.setItem('products', JSON.stringify([]));
-        let product1 = new Product('product1', 'description1', 100, 10, 5, true, 'image1', new Date(), new Date());
-        let product2 = new Product('product2', 'description2', 200, 20, 4, true, 'image2', new Date(), new Date());
-        let product3 = new Product('product3', 'description3', 300, 30, 3, true, 'image3', new Date(), new Date());
-        let product4 = new Product('product4', 'description4', 400, 0, 2, false, 'image4', new Date(), new Date());
-        let product5 = new Product('product5', 'description5', 500, 50, 1, true, 'image5', new Date(), new Date());
-        let product6 = new Product('product6', 'description6', 600, 60, 5, true, 'image6', new Date(), new Date());
+        let product1 = new Product('product1', 'description1', 100, 10, 5, true, 'https://via.placeholder.com/200', new Date(), new Date());
+        let product2 = new Product('product2', 'description2', 200, 20, 4, true, 'https://via.placeholder.com/200', new Date(), new Date());
+        let product3 = new Product('product3', 'description3', 300, 30, 3, true, 'https://via.placeholder.com/200', new Date(), new Date());
+        let product4 = new Product('product4', 'description4', 400, 0, 2, false, 'https://via.placeholder.com/200', new Date(), new Date());
+        let product5 = new Product('product5', 'description5', 500, 50, 1, true, 'https://via.placeholder.com/200', new Date(), new Date());
+        let product6 = new Product('product6', 'description6', 600, 60, 5, true, 'https://via.placeholder.com/200', new Date(), new Date());
 
         product1.createProduct();
         product2.createProduct();
@@ -33,7 +33,7 @@ function displayProducts(array : any) {
             <div class="product">
                 <h2>${product._title}</h2>
                 <div>
-                <img src="${product._image}" alt="${product._title}">
+                <img src="${product._image}" alt="${product._title}" loading="lazy">
                 <ul>
                     <li class="available"></li>
                     <li>${product._price} $(USD)</li>
